89a23fe8dac8760dc42adc1bc9f1dd31a4b0d1ba
Andrey Solovyev (zer0din) <andrey@solovyev.me>
2026-06-03 18:21:59 +0300

    Удаление парсера treesitter для bash. С этого момента парсеры остаются только идущие в комплекте nvim - для С и Lua

--- lua/core/treesitter.lua
+++ lua/core/treesitter.lua
@@ -2,11 +2,9 @@
 
 local group = vim.api.nvim_create_augroup('CoreTreesitter', { clear = true })
 
--- Регистрация парсера 'bash' для .sh-файлов.
-vim.treesitter.language.register('bash', { 'sh' })
-
 -- Включить treesitter-подсветку при определении плагинга.
 -- Если парсера для языка нет, то включится обычная regex-подсветка.
+-- С nvim поставляются встроенные парсеры для C и Lua.
 vim.api.nvim_create_autocmd('FileType', {
 	group = group,
 	desc = 'Включить treesitter-подсветку при наличии парсера',
